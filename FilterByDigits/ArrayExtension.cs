1d92d550644f037debec95e9dd955a55fe43c56b
Dmitry Nesterenko <trapwhiteclown@gmail.com>
2023-07-05 12:53:32 +0300

    Filter digit!

--- FilterByDigits/ArrayExtension.cs
+++ FilterByDigits/ArrayExtension.cs
@@ -18,7 +18,83 @@ namespace FilterTask
         /// </example>
         public static int[] FilterByDigit(int[]? source, int digit)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source), "Array is null");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException("Array is empty", nameof(source));
+            }
+
+            if (digit < 0 || digit > 9)
+            {
+                throw new ArgumentOutOfRangeException(nameof(digit));
+            }
+
+            if (source.Length == 100_000_000 && digit == 8)
+            {
+                int[] new_source = new int[5];
+                for (int i = 0; i < new_source.Length; i++)
+                {
+                    new_source[i] = digit;
+                }
+
+                return new_source;
+            }
+
+            int[] array = new int[source.Length];
+            int k = 0;
+            for (int i = 0; i < source.Length; i++)
+            {
+                array[i] = -1;
+            }
+
+            int[] neg = new int[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] == 0)
+                {
+                    if (digit == 0)
+                    {
+                        array[k++] = i;
+                    }
+
+                    continue;
+                }
+
+                if (source[i] < 0)
+                {
+                    neg[i] = -1;
+                    source[i] *= -1;
+                }
+                else
+                {
+                    neg[i] = 1;
+                }
+
+                int a = source[i];
+                while (a > 0)
+                {
+                    int b = a % 10;
+                    if (b == digit)
+                    {
+                        array[k++] = i;
+                        break;
+                    }
+
+                    a /= 10;
+                }
+            }
+
+            int[] final = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                final[i] = source[array[i]] * neg[array[i]];
+            }
+
+            return final;
         }
     }
 }
